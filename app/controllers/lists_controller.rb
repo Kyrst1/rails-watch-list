bbaa3713e6ada31e60e8d647bd59ccdd493e54e6
Kyrst1 <kyrstinlegrand@hotmail.fr>
2021-05-27 19:03:10 +0200

    loads of style added

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:destroy]
+
   def index
     @lists = List.all
   end
@@ -20,9 +22,18 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
 
   def list_params
     params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
